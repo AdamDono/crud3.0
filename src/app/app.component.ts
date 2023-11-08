98285483268213e365241516e0d4b1eb09a4e79d
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-11-08 11:32:25 +0200

    get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit  {
   title = 'crud3.0';
 
 
-constructor(private _dialog: MatDialog) {}
+constructor(private _dialog: MatDialog, private _empService: EmployeeService ) {}
+
+ngOnInit(): void {
+   this.getEmployeeList();
+}
 
 openAddEditEmpForm() 
 //add component you want to pass the method on
@@ -19,6 +24,16 @@ openAddEditEmpForm()
   this._dialog.open (EmpAddEditComponent);
 } 
 
-}
+getEmployeeList ( ) {
+  this._empService.getEmployeeList().subscribe({
+    next: (res) => {
+    console.log(res);
   
+    },
+  error: console.log,
+   
+ } );    
+}
+}
+ 
 
